c66b43b640809049d3e1d4e4f26d67f3257a6d72
sagnikc395 <sagnikchatterjee607@gmail.com>
2025-07-01 22:49:45 +0530

    feat: sent correlation id

--- app/main.py
+++ app/main.py
@@ -1,4 +1,23 @@
 import socket  # noqa: F401
+import struct
+
+class KafkaResponseMessage:
+    def __init__(self,header,body=None):
+        # creating the message_header as a 32 bit(signed) with 0 value
+        self.message_head = 0
+        self.header = header
+        self.body = body
+
+    def get_message_head(self):
+        return self.message_head
+
+    def send_header(self,conn):
+    # send the message_head and header as a response
+    # both message_head and header will be packed as 32-bit big-endian
+        packed_data = struct.pack(">II",self.message_head,self.header)
+        conn.send(packed_data)
+
+
 
 
 def main():
@@ -9,8 +28,13 @@ def main():
     # Uncomment this to pass the first stage
     #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    conn,addr = server.accept() # wait for client
+    while conn:
+        print(f"Connected by {addr}")
+        message = KafkaResponseMessage(header=7)
+        message.send_header(conn)
 
+    server.close()
 
 if __name__ == "__main__":
     main()
